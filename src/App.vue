e1f989c9d9340187a76816398575b513c5ca15b0
Mateusz Werpachowski <matyszxzx@gmail.com>
2019-06-17 19:53:42 +0200

    added firebase auth

--- src/App.vue
+++ src/App.vue
@@ -1,261 +1,33 @@
 <template>
   <div id="app">
-    <canvas id="canvas" height="500" width="500"></canvas>
+    <div v-if="user">
+      <Game :user="user"/>
+      <button @click="signOut()">Logout</button>
+    </div>
+    <div v-else>
+      <button @click="login()">Login</button>
+    </div>
   </div>
 </template>
 
 <script>
-import Node from "./node";
-import Player from "./player";
+import Game from "./Game";
+import { auth, googleProvider } from "./firebase";
 
 export default {
   name: "app",
+  components: { Game },
   data() {
     return {
-      canvas: null,
-      ctx: null,
-      mazeSize: 10,
-      nodeSize: null,
-      maze: null,
-      player: null,
-      directions: {
-        LEFT: 0b0001,
-        RIGHT: 0b0010,
-        UP: 0b0100,
-        DOWN: 0b1000
-      }
+      user: null
     };
   },
-  mounted() {
-    this.canvas = document.getElementById("canvas");
-    this.ctx = this.canvas.getContext("2d");
-
-    this.nodeSize = 500 / this.mazeSize;
-
-    this.init();
-    window.addEventListener("keydown", e => {
-      const key = e.keyCode;
-      this.checkIfCorrectKeyAndMovePlayer(key).then(this.checkIfWon);
-    });
-  },
   methods: {
-    init() {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-      this.initNodes();
-      this.draw();
-      this.dfs(0, 0);
-      this.initPlayer();
+    login() {
+      auth.signInWithPopup(googleProvider).then(res => (this.user = res.user));
     },
-    initNodes() {
-      this.maze = [];
-      for (let y = 0; y < this.mazeSize; y++) {
-        this.maze[y] = [];
-        for (let x = 0; x < this.mazeSize; x++) {
-          this.maze[y][x] = new Node(x, y, false);
-        }
-      }
-    },
-    draw() {
-      this.ctx.strokeStyle = "#000";
-      this.ctx.lineWidth = 2;
-      this.ctx.beginPath();
-      for (let i = 0; i <= this.mazeSize; i++) {
-        this.ctx.moveTo(0, i * this.nodeSize);
-        this.ctx.lineTo(500, i * this.nodeSize);
-        this.ctx.stroke();
-
-        this.ctx.moveTo(i * this.nodeSize, 0);
-        this.ctx.lineTo(i * this.nodeSize, 500);
-        this.ctx.stroke();
-      }
-    },
-    dfs(x, y, parent) {
-      const node = this.maze[y][x];
-      console.log(node);
-
-      if (node.visited) return false;
-      else {
-        if (parent) {
-          node.parent = parent;
-          this.breakWall(node);
-        }
-        node.visited = true;
-      }
-
-      let visitedDirs = [];
-      while (1) {
-        let direction = this.srand(node, visitedDirs);
-        console.log(direction);
-        if (!direction) break;
-        if (direction == this.directions.LEFT) {
-          this.dfs(x - 1, y, node);
-        }
-        if (direction == this.directions.RIGHT) {
-          this.dfs(x + 1, y, node);
-        }
-        if (direction == this.directions.UP) {
-          this.dfs(x, y - 1, node);
-        }
-        if (direction == this.directions.DOWN) {
-          this.dfs(x, y + 1, node);
-        }
-        visitedDirs.push(direction);
-      }
-      return true;
-    },
-    breakWall(node) {
-      const parent = node.parent;
-      const { point0, point1, direction } = this.getWallToBreak(node, parent);
-      const invertedDirection =
-        direction === this.directions.UP
-          ? this.directions.DOWN
-          : direction === this.directions.DOWN
-          ? this.directions.UP
-          : direction === this.directions.LEFT
-          ? this.directions.RIGHT
-          : this.directions.LEFT;
-
-      parent.brokenWalls.push(direction);
-      node.brokenWalls.push(invertedDirection);
-
-      this.ctx.strokeStyle = "#fff";
-      this.ctx.lineWidth = 4;
-      this.ctx.beginPath();
-      this.ctx.moveTo(point0.x + 1, point0.y + 1);
-      this.ctx.lineTo(point1.x - 1, point1.y - 1);
-      this.ctx.stroke();
-    },
-    getWallToBreak(node, parent) {
-      const differentDimension = ["x", "y"].filter(
-        dim => node[dim] !== parent[dim]
-      )[0];
-
-      const isWallHorizontal = differentDimension === "y";
-      const isNodeFurther =
-        node[differentDimension] > parent[differentDimension];
-
-      let point0 = {};
-      let point1 = {};
-      let direction = null;
-
-      if (isNodeFurther) {
-        point0 = {
-          x: node.x * this.nodeSize,
-          y: node.y * this.nodeSize
-        };
-        if (isWallHorizontal) {
-          direction = this.directions.DOWN;
-          point1 = {
-            x: node.x * this.nodeSize + this.nodeSize,
-            y: node.y * this.nodeSize
-          };
-        } else {
-          direction = this.directions.RIGHT;
-          point1 = {
-            x: node.x * this.nodeSize,
-            y: node.y * this.nodeSize + this.nodeSize
-          };
-        }
-      } else {
-        point0 = {
-          x: parent.x * this.nodeSize,
-          y: parent.y * this.nodeSize
-        };
-        if (isWallHorizontal) {
-          direction = this.directions.UP;
-          point1 = {
-            x: parent.x * this.nodeSize + this.nodeSize,
-            y: parent.y * this.nodeSize
-          };
-        } else {
-          direction = this.directions.LEFT;
-          point1 = {
-            x: parent.x * this.nodeSize,
-            y: parent.y * this.nodeSize + this.nodeSize
-          };
-        }
-      }
-      const wall = { point0, point1, direction };
-      return wall;
-    },
-    srand(node, visitedDirs) {
-      const directionsArray = Object.values(this.directions);
-      let leftDirs = directionsArray.filter(dir => !visitedDirs.includes(dir));
-
-      if (node.x === 0)
-        leftDirs.splice(leftDirs.indexOf(this.directions.LEFT), 1);
-      if (node.x === this.mazeSize - 1)
-        leftDirs.splice(leftDirs.indexOf(this.directions.RIGHT), 1);
-      if (node.y === 0)
-        leftDirs.splice(leftDirs.indexOf(this.directions.UP), 1);
-      if (node.y === this.mazeSize - 1)
-        leftDirs.splice(leftDirs.indexOf(this.directions.DOWN), 1);
-
-      if (leftDirs.length === 1) return leftDirs[0];
-
-      const r = Math.floor(Math.random() * leftDirs.length);
-      return leftDirs[r];
-    },
-    initPlayer() {
-      if (this.player) this.player.clear();
-
-      const color = this.getRandomColor();
-      this.player = new Player(0, 0, this.nodeSize, color, this.ctx);
-    },
-    getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    },
-    checkIfCorrectKeyAndMovePlayer(key) {
-      const self = this;
-      return new Promise((resolve, reject) => {
-        const x = self.player.x;
-        const y = self.player.y;
-        const node = self.maze[y][x];
-        const brokenWalls = node.brokenWalls;
-
-        if (key == 37) {
-          if (x !== 0 && brokenWalls.includes(self.directions.LEFT)) {
-            self.player.goLeft();
-            resolve();
-          }
-        } else if (key == 39) {
-          if (
-            x !== self.mazeSize - 1 &&
-            brokenWalls.includes(self.directions.RIGHT)
-          ) {
-            self.player.goRight();
-            resolve();
-          }
-        } else if (key == 38) {
-          if (y !== 0 && brokenWalls.includes(self.directions.UP)) {
-            self.player.goUp();
-            resolve();
-          }
-        } else if (key == 40) {
-          if (
-            y !== self.mazeSize - 1 &&
-            brokenWalls.includes(self.directions.DOWN)
-          ) {
-            self.player.goDown();
-            resolve();
-          }
-        } else reject("Invalid key");
-      });
-    },
-    checkIfWon() {
-      if (
-        this.player.x === this.mazeSize - 1 &&
-        this.player.y === this.mazeSize - 1
-      ) {
-        console.log("You won!");
-        setTimeout(this.init, 1000);
-      }
+    signOut() {
+      auth.signOut().then(res => (this.user = null));
     }
   }
 };
@@ -266,9 +38,5 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-#canvas {
-  border: 1px solid #000;
 }
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8fRk5udVuwZvX1M6-pWRNWoYFXdef9vk",
+    authDomain: "maze-733de.firebaseapp.com",
+    databaseURL: "https://maze-733de.firebaseio.com",
+    projectId: "maze-733de",
+    storageBucket: "maze-733de.appspot.com",
+    messagingSenderId: "719616225065",
+    appId: "1:719616225065:web:a41fa2ef598c6ba8"
+};
+console.log(firebase);
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export const db = firebase.firestore();
